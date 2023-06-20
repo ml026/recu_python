c9c9c0c20ca99bca3449ac83dd3a25c428e9248e
ml026 <maria.lahuerta.m@gmail.com>
2023-06-20 11:31:38 +0200

    cOMMIT4

--- examen.py
+++ examen.py
@@ -1,5 +1,6 @@
-#Ej 1
+import csv
 
+#Ej 1
 def mediana(diccionario):
     valores = list(diccionario.values())
     ordenados = sorted(valores)
@@ -50,25 +51,46 @@ print(diccionario_final)
 
 #Ej4
 def palabra_repetida(fichero):
-    contador = {}
-    p_mas_repetida = None
+    contador={}
+    p_mas_repetida= None   
     max_repeticiones = 0
-    with open (fichero,'r') as archivo:
+    
+    with open(fichero, 'r') as archivo:
         for linea in archivo:
-            palabra=linea.strip()
-        if palabra in contador:
-            contador[palabra]= ( contador[palabra]) +1
+            palabra = linea.strip().lower()             
+            if palabra in contador:
+                contador[palabra] += 1
+            else:
+                contador[palabra] = 1
+                
+            if contador[palabra] > max_repeticiones:
+                max_repeticiones = contador[palabra]
+                p_mas_repetida = palabra
+    
+    return p_mas_repetida
 
-        if contador[palabra]>max_repeticiones:
-            max_repeticiones=contador[palabra]
-            p_mas_repetida=palabra
-            return p_mas_repetida
-        
-fichero="palabras.txt"
-p_mas_repetida=palabra_repetida(fichero)
+fichero = "palabras.txt"
+p_mas_repetida = palabra_repetida(fichero)
 print("La palabra más repetida es:", p_mas_repetida)
 
+#Ej2
+def procesar (datos,n_curso):
+ estudiantes = []
 
+ with open(datos,'r') as archivo:
+     csv_read=csv.reader(archivo)
+     next(csv_read)
 
-    
-    
+     for linea in csv_read:
+         nombre=linea[0]
+         apellido=linea[1]
+         edad=linea[2]
+         curso=linea[3]
+
+         if curso == str(n_curso):
+             estudiantes.append(nombre,apellido,edad,curso)
+
+       if len(estudiantes)==0:
+         raise ValueError("No hay estudiantes")
+     return estudiantes
+             
